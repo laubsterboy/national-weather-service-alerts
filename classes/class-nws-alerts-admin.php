c4b8a7407de5122235f5ebec914cd3ea140ef561
John Russell <laubsterboy@gmail.com>
2014-11-08 19:47:26 -0700

    updated admin settings page

--- classes/class-nws-alerts-admin.php
+++ classes/class-nws-alerts-admin.php
@@ -298,7 +298,7 @@ class NWS_Alerts_Admin {
                     $return_value .= '<table>';
                         $return_value .= '<tbody>';
 
-                            $return_value .= self::get_control('alerts_bar', $control_id_prefix);
+                            $return_value .= self::get_control('enabled', $control_id_prefix);
                             $return_value .= self::get_control('nonce', $control_id_prefix);
                             $return_value .= self::get_control('zip', $control_id_prefix);
                             $return_value .= self::get_control('city', $control_id_prefix);
