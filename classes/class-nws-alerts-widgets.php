3c9fa2e5278a9f5c36597fafa9ae6aa5d53fe297
John Russell <laubsterboy@gmail.com>
2014-11-20 18:42:31 -0700

    widget output including args

--- classes/class-nws-alerts-widgets.php
+++ classes/class-nws-alerts-widgets.php
@@ -40,8 +40,9 @@ class NWS_Alerts_Widget extends WP_Widget {
         if (!empty($instance['zip']) || (!empty($instance['city']) && !empty($instance['state'])) || (!empty($instance['state']) && !empty($instance['county']))) {
             $nws_alert_data = new NWS_Alerts(array('zip' => $instance['zip'], 'city' => $instance['city'], 'state' => $instance['state'], 'county' => $instance['county'], 'scope' => $instance['scope']));
 
+            echo $args['before_widget'];
             echo $nws_alert_data->get_output_html($instance['display'], 'nws-alerts-widget');
-
+            echo $args['after_widget'];
             unset($nws_alert_data);
         }
 	}
