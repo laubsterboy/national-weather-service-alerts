330b8fe91a2ae3753f03e3791ef3fe0eecb81a3c
John Russell <laubsterboy@gmail.com>
2014-11-23 17:21:05 -0700

    Fixed syntax error

--- uninstall.php
+++ uninstall.php
@@ -16,16 +16,16 @@ $table_name = NWS_ALERTS_TABLE_NAME_CODES;
 $wpdb->query("DROP TABLE IF EXISTS $table_name");
 
 // Remove options
-remove_option('nws_alerts_alerts_bar_enabled');
-remove_option('nws_alerts_alerts_bar_zip');
-remove_option('nws_alerts_alerts_bar_city');
-remove_option('nws_alerts_alerts_bar_state');
-remove_option('nws_alerts_alerts_bar_county');
-remove_option('nws_alerts_alerts_bar_scope');
-remove_option('nws_alerts_alerts_bar_fix');
+delete_option('nws_alerts_alerts_bar_enabled');
+delete_option('nws_alerts_alerts_bar_zip');
+delete_option('nws_alerts_alerts_bar_city');
+delete_option('nws_alerts_alerts_bar_state');
+delete_option('nws_alerts_alerts_bar_county');
+delete_option('nws_alerts_alerts_bar_scope');
+delete_option('nws_alerts_alerts_bar_fix');
 
-remove_site_option('nws_alerts_version');
-remove_site_option('nws_alerts_tables_built');
+delete_site_option('nws_alerts_version');
+delete_site_option('nws_alerts_tables_built');
 
 // Remove any lingering transients - should never exist if activation and setup ran properly.
 delete_site_transient('nws_alerts_populate_tables_args');
