7d7d061aedd9692af1f46313d6a6a6a07c515565
John Russell <laubsterboy@gmail.com>
2014-12-02 21:45:48 -0700

    Adjusted formatting

--- classes/class-nws-alerts-entry.php
+++ classes/class-nws-alerts-entry.php
@@ -234,7 +234,7 @@ class NWS_Alerts_Entry {
 
         // Size
 		if ($details) {
-            $return_value = '<span class="nws-alerts-event nws-alerts-color-' . $this->cap_event_slug . '">' . $this->cap_event . '</span><br /> ' . ucwords(strtolower($this->summary)) . ' ...<a href="' . $this->id . '">Read More</a>.<br /><span class="nws-alerts-event-effective">' . $this->cap_effective . '</spa> - <span class="nws-alerts-event-expires">' . $this->cap_expires . '</span>';
+            $return_value = '<span class="nws-alerts-event nws-alerts-color-' . $this->cap_event_slug . '">' . $this->cap_event . '</span><br /> ' . ucwords(strtolower($this->summary)) . ' ...<a href="' . $this->id . '">Read More</a>.<br /><span class="nws-alerts-event-effective"><strong>Effective:</strong> ' . $this->cap_effective . '</spa> - <span class="nws-alerts-event-expires"><strong>Expires:</strong> ' . $this->cap_expires . '</span>';
         } else {
             $return_value = '<span class="nws-alerts-event nws-alerts-color-' . $this->cap_event_slug . '">' . $this->cap_event . '</span>';
         }
